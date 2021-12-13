98956719e000b5fb08eabddcda43dc01c28c5519
athieffry <axel.thieffry@gmail.com>
2021-12-13 12:57:00 +0100

    improve code formatting

--- Day_13/solutions.R
+++ Day_13/solutions.R
@@ -22,36 +22,33 @@ df <- input %>%
       mutate_all(~add(., 1)) %>%
       set_colnames(c('x', 'y'))
 
-# b. create initial matrix
-    # empty matrix of max dimensions
-    max_x <- max(df$x)
-    max_y <- max(df$y)
-
-    mat <- matrix(rep(0, max_x * max_y), ncol=max_x)
+# b. create initial empty matrix with max dimensions encountered
+max_x <- max(df$x)
+max_y <- max(df$y)
+mat <- matrix(rep(0, max_x * max_y), ncol=max_x)
 
 # c. populate initial matrix
 apply(df, 1, \(x) mat[x[2], x[1]] <<- 1 )
-dim(mat)
 
 # d. folding functions
 fold_y <- function(mat, yline) {
-    # get up matrix
+    # get upper matrix
     up <- mat[1:(yline-1), ]
     dim(up)
-    # get down matrix
+    # get lower matrix
     down <- mat[(yline+1):nrow(mat), ] %>% apply(2, rev)
     dim(down)
     # get row number delta
     delta <- nrow(up) - nrow(down)
         # CASE 1: both matrix of same height
         if(delta==0) { return(up + down) }
-        # CASE 2: up matrix is bigger
+        # CASE 2: upper matrix is bigger
         if(delta > 0) {
                        buffer <- matrix(rep(0, ncol(mat) * abs(delta)), ncol=ncol(mat))
                        down <- rbind(buffer, down)
                        return(up + down)
                       }
-        # CASE 3: up matrix is smaller
+        # CASE 3: upper matrix is smaller
         if(delta < 0) {
                        buffer <- matrix(rep(0, ncol(mat) * abs(delta)), ncol=ncol(mat))
                        up <- rbind(up, buffer)
@@ -65,7 +62,7 @@ fold_x <- function(mat, xline) {
     left <- mat[ ,1:(xline-1)]
     # get right matrix
     right <- mat[ ,(xline+1):ncol(mat)] %>% apply(1, rev) %>% t()
-    # get col number delta
+    # get column number delta
     delta <- ncol(left) - ncol(right)
         # CASE 1: both matrix of same width
         if(delta==0) { return(left + right) }
@@ -83,19 +80,20 @@ fold_x <- function(mat, xline) {
         }
 }
 
-# e. Q1
+# e. Question 1:
 mat_q1 <- mat
 apply(foldings[1,], 1, \(x) ifelse(x[1]=='x',
                                    mat_q1 <<- fold_x(mat_q1, xline=as.numeric(x[2])),
                                    mat_q1 <<- fold_y(mat_q1, yline=as.numeric(x[2]))) )
 sum(mat!=0)
 
-# f. Q2
+# f. Question 2:
 apply(foldings, 1, \(x) {
                         ifelse(x[1]=='x',
                                mat <<- fold_x(mat, xline=as.numeric(x[2])),
                                mat <<- fold_y(mat, yline=as.numeric(x[2])))
                         })
 
+# g. plot password
 mat[mat > 1] <- 1
-pheatmap::pheatmap(mat2, cluster_rows=F, cluster_cols=F, cellwidth=5, cellheight=5, color=c('white', 'black'), legend=F)
+pheatmap::pheatmap(mat2, cluster_rows=F, cluster_cols=F, cellwidth=5, cellheight=5, color=c('white', 'black'), legend=F, border_color=NA)
